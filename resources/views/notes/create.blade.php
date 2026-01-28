6ffa0322175901c4a886bbf2756961493485127e
joel12-Sant <joelsantiagos000@gmail.com>
2026-01-27 20:37:32 -0600

    feat: support optional note fields

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -3,5 +3,15 @@
 @section('title', 'Crear Nota | NotesTips')
 
 @section('main-content')
-    @include('partials.notes.create-form')
+    <div class="h-full flex overflow-hidden">
+        <aside class="w-80 shrink-0 border-r border-slate-200 bg-white overflow-auto">
+            @include('partials.notes.list')
+        </aside>
+
+        <section class="flex-1 overflow-auto bg-slate-50">
+            <div class="max-w-4xl mx-auto p-8">
+                @include('partials.notes.create-form')
+            </div>
+        </section>
+    </div>
 @endsection
--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -1,7 +1,23 @@
 @extends('layout.notes.app')
 
-@section('title', 'NotesTips')
+@section('title', 'Mis notas | NotesTips')
 
 @section('main-content')
-    <h1>Notas Principal</h1>
+    <div class="h-full flex overflow-hidden">
+        <aside class="w-80 shrink-0 border-r border-slate-200 bg-white overflow-auto">
+            @include('partials.notes.list')
+        </aside>
+
+        <section class="flex-1 overflow-auto bg-slate-50">
+            <div class="max-w-4xl mx-auto p-8">
+                @isset($note)
+                    {{-- @include('partials.notes.show', ['note' => $note]) --}}
+                @else
+                    <div class="h-full flex items-center justify-center text-slate-500">
+                        Selecciona una nota para ver su contenido
+                    </div>
+                @endisset
+            </div>
+        </section>
+    </div>
 @endsection
