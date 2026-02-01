1153213f7c7d507bc6ce9d5309cd6c5c5ef03fbb
joel12-Sant <joelsantiagos000@gmail.com>
2026-02-01 15:44:05 -0600

    add pages in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,4 +4,14 @@ import './bootstrap';
 
 import Alpine from 'alpinejs'
 window.Alpine = Alpine
-Alpine.start()
+
+const pages = import.meta.glob('./pages/*.js');
+
+const page = document.body?.dataset?.page || null;
+const key = `./pages/${page}.js`;
+
+if (page && pages[key]) {
+  pages[key]().catch(() => {
+
+  });
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,6 @@
 
 @section('main-content')
     <main>
-        <h1 class="text-red-500 bg-amber-50">Index</h1>
-        <div class="p-6 bg-red-500 text-white font-bold">Tailwind vive</div>
+        <h1 class="text-red-500 bg-amber-50"> /notes </h1>
     </main>
 @endsection
